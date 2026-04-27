301b7c0c3f333fcc2560ddb2df5b0c2d168da12a
Muhammad Ahsan <malikmuhammadahsan21@gmail.com>
2026-04-27 12:18:47 -0700

    fix: hardcode logo path so it works on Vercel

--- frontend/src/data/common/siteDetails.ts
+++ frontend/src/data/common/siteDetails.ts
@@ -7,6 +7,6 @@ export const siteDetails = {
     },
     language: 'en-us',
     locale: 'en-US',
-    siteLogo: `${process.env.BASE_PATH || ''}/images/122.png`, // or use a string for the logo e.g. "TechStartup"
+    siteLogo: '/images/122.png',
     googleAnalyticsId: '', // e.g. G-XXXXXXX,
 }
