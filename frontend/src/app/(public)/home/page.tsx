2657e847f676dfb53a59c4338b3351a8607491ed
Muhammad Ahsan <malikmuhammadahsan21@gmail.com>
2026-05-15 17:07:50 +0500

    Hide sections on desktop view: Hero, Section43 statistics, and Contact sections

--- frontend/src/app/(public)/home/page.tsx
+++ frontend/src/app/(public)/home/page.tsx
@@ -1,11 +1,10 @@
-import About from "@/components/home/About";
 import Hero from "@/components/home/Hero";
-import Section43 from "@/components/home/Section43";
 import Sliderauto from "@/components/home/sliderauto";
+import Section43 from "@/components/home/Section43";
 import Model from "@/components/home/Model";
 import DevSecSlider from "@/components/home/DevSecSlider";
-import Blogs from "@/components/home/Blogs";
 import OurProjects from "@/components/home/OurProjects";
+import Blogs from "@/components/home/Blogs";
 import Testimonials from "@/components/home/Testimonials";
 import FAQ from "@/components/home/FAQ";
 import Cont from "@/components/home/Cont";
@@ -13,15 +12,24 @@ import Cont from "@/components/home/Cont";
 export default function HomePage() {
     return (
         <>
-            <Hero />
+            {/* Hero section - hidden on desktop */}
+            <div className="hidden md:block">
+                <Hero />
+            </div>
             <Sliderauto />
-            <Section43 />
+            {/* Statistics section - hidden on desktop */}
+            <div className="hidden md:block">
+                <Section43 />
+            </div>
             <Model />
             <DevSecSlider />
             <OurProjects />
             <Blogs />
             <Testimonials />
-            <Cont />
+            {/* Contact section - hidden on desktop */}
+            <div className="hidden md:block">
+                <Cont />
+            </div>
             <FAQ />
         </>
     );
